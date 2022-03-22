9df32ecd35d297733fad3ad02f110ddab6497cef
David Vilela <dvilelaf@gmail.com>
2022-03-22 13:28:05 +0100

    fix: rename liquidity_provision skill

--- packages/valory/skills/liquidity_rebalancing_abci/__init__.py
+++ packages/valory/skills/liquidity_rebalancing_abci/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------------------------
 #
-#   Copyright 2021 Valory AG
+#   Copyright 2021-2022 Valory AG
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -17,9 +17,9 @@
 #
 # ------------------------------------------------------------------------------
 
-"""This module contains the liquidity provision skill for an AEA."""
+"""This module contains the liquidity rebalancing skill for an AEA."""
 
 from aea.configurations.base import PublicId
 
 
-PUBLIC_ID = PublicId.from_str("valory/liquidity_provision:0.1.0")
+PUBLIC_ID = PublicId.from_str("valory/liquidity_rebalancing_abci:0.1.0")
